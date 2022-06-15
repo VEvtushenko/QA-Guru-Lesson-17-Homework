3208ee68bc2a02b197fedb5e987b524367efd30c
VEvtushenko <evt-vladimir@yandex.ru>
2022-06-15 18:19:07 +0300

    fix TestData

--- src/test/java/guru/qa/tests/TestData.java
+++ src/test/java/guru/qa/tests/TestData.java
@@ -1,20 +1,24 @@
 package guru.qa.tests;
 
 public class TestData {
+
     public static String updateUserJson = "{\n" +
             "    \"name\": \"Chzu Bajie\",\n" +
             "    \"job\": \"Heavenly Tumbleweed\",\n" +
             "    \"type\": \"Human-pig\",\n" +
             "    \"Buddhist_name\": \"Zhu Wuneng\"" +
             "}";
+
     public static String createUserJson = "{\\n\" +\n" +
             "                        \"    \\\"name\\\": \\\"Chzu Bajie\\\",\\n\" +\n" +
             "                        \"    \\\"job\\\": \\\"looser\\\",\\n\" +\n" +
             "                        \"    \\\"type\\\": \\\"Human-pig\\\"\" +\n" +
             "                        \"}";
+
     public static String registerUserJson = "{\n" +
             "    \"email\": \"czhu.bajie@reqres.in\",\n" +
             "    \"password\": \"pistol\"\n" +
             "}";
+
     public static String userJanetWeaver = "";
 }
